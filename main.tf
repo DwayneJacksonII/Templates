034d8c1b4af0065b0d4610c01fb0a883a44722f4
Dwayne Jackson II <81774183+DwayneJacksonII@users.noreply.github.com>
2025-01-22 18:05:43 -0500

    Update main.tf
    Dwaynej 1.22.25 creation of a resource group, virtual network, and subnet, and prompts the user to select a region

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@
 # 1.0.0 - Initial version 1/16/25
 #1.0.1 - Updated for Azure Government use will need to verify the exact API versions supported in your Azure Government subscription.
 #1.0.2 - commenting out Environment use for gov updating v1/22/25- dj
+#1.0.3 - creation of a resource group, virtual network, and subnet, and prompts the user to select a region 
+#All resources including the private endpoint are placed in the created resource group
 
 # main.tf
 terraform {
@@ -22,58 +24,73 @@ provider "azurerm" {
 
 # Variables
 variable "location" {
-  description = "Location for the private endpoint"
-  default     = null
+  description = "Location for the resources"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
 }
 
 variable "private_endpoint_name" {
   description = "Name of the private endpoint"
+  type        = string
 }
 
 variable "target_resource_id" {
   description = "Resource ID of the Azure resource for which the private endpoint needs to be created"
+  type        = string
 }
 
 variable "group_id" {
   description = "Subresource name of the Azure resource (e.g., 'sites' for Function App)"
+  type        = string
 }
 
 variable "vnet_name" {
-  description = "Virtual network name where private endpoint will be created"
+  description = "Virtual network name"
+  type        = string
 }
 
 variable "subnet_name" {
-  description = "Subnet name where private endpoint will be created"
+  description = "Subnet name"
+  type        = string
 }
 
 variable "private_dns_zone_id" {
   description = "Private DNS Zone Resource ID"
+  type        = string
 }
 
-# Data Sources
-data "azurerm_virtual_network" "vnet" {
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-data "azurerm_subnet" "subnet" {
+# Subnet
+resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  virtual_network_name = data.azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-}
-
-# Resource Group (example resource group, replace as needed)
-resource "azurerm_resource_group" "rg" {
-  name     = "example-resource-group"
-  location = var.location != null ? var.location : "eastus"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Private Endpoint
 resource "azurerm_private_endpoint" "private_endpoint" {
   name                = var.private_endpoint_name
-  location            = var.location != null ? var.location : azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = data.azurerm_subnet.subnet.id
+  subnet_id           = azurerm_subnet.subnet.id
 
   private_service_connection {
     name                           = var.private_endpoint_name
@@ -91,7 +108,7 @@ resource "azurerm_private_endpoint" "private_endpoint" {
 resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_link" {
   name                 = "example-dns-link"
   private_dns_zone_id  = var.private_dns_zone_id
-  virtual_network_id   = data.azurerm_virtual_network.vnet.id
+  virtual_network_id   = azurerm_virtual_network.vnet.id
   resource_group_name  = azurerm_resource_group.rg.name
 
   registration_enabled = true
